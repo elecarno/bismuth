ac431873fb4c8530d805778bdeb0a855f4099e03
Elecarno <54546385+ElecarnoYT@users.noreply.github.com>
2021-12-29 16:43:22 +0000

    alpha 1.2 dev 1

--- entities/entity.js
+++ entities/entity.js
@@ -1,27 +1,72 @@
+const { send } = require('express/lib/response')
+
 require('../world')
 
 world = new World()
 
-var colTiles = [3]
+var tpd = 50 // tile pixel dimension
+var ctd = 32 // chunk tile dimension
+
+var colTiles = [3, 7, 8, 11, 14, 17]
 var intTiles = [7]
 var autoGuns = ["shroom_k"]
 var singleGuns = ["hunting_rifle"]
 var meleeWeapons = ["survival_knife"]
-var miningTools = ["bronze_pickaxe", "iron_pickaxe"]
-var placeableItems = ["rock", "rocky_floor"]
-var priorityTiles = [7]
+var miningTools = ["bronze_pickaxe", "iron_pickaxe", "iron_drill"]
+var harvestTools = ["survival_knife", "bronze_sickle"]
+var workTools = ["bronze_chisel"]
+var placeableItems = [
+"rock", "rocky_floor", "granite", "earth", "beq_rock", 
+"stone", "organic_floor", "dirt_floor", "cave_flower", "toad_shroom", 
+"pollen_shroom", "bronze_berry", "mound"
+]
+var priorityTiles = [7, 3, 7, 8, 11, 14, 17]
 var placeIds = {
     "rock" : 3,
     "rocky_floor" : 2,
+    "granite": 8,
+    "organic_floor": 13,
+    "beq_rock": 14,
+    "dirt_floor": 10,
+    "earth": 11,
+    "stone": 15,
+    "cave_flower": 6,
+    "toad_shroom": 4,
+    "pollen_shroom": 5,
+    "bronze_berry": 16,
+    "mound":18,
 }
-var miningToolStrengths = { // must be integer
-    "bronze_pickaxe": 1,
+var miningToolStrengths = {
+    "bronze_pickaxe": 1.5,
     "iron_pickaxe": 2,
+    "iron_drill": 7,
 }
+var harvestToolStrengths = {
+    "survival_knife": 0.5,
+    "bronze_sickle": 1,
+}
+var workToolStrengths = {
+    "bronze_chisel": 1,
+}
+var tileStrengths = {
+    2: 20, 3: 40, 4: 5, 5: 5, 6: 5, 7: 100,
+    8: 55, 10: 20, 11: 30, 13: 20, 14: 50, 15: 5,
+    16: 10, 17: 50, 18: 55
+}
+var mineTiles = [2, 3, 8, 13, 14, 10, 11, 17, 18]
+var harvestTiles = [4, 5, 6, 15, 16]
+var workTiles = [7]
+
+var floor1Tiles = [1,2,3,4,5,6,7,8,17]
+var floor2Tiles = [12,13,14,16]
+var floor3Tiles = [9,10,11,15,18]
 
 craftingRecipes = [
-    ["medkit", "adrenaline", "cave_beef"],
-    ["rock", "rocky_floor", "almond_water"]
+    ["toad_shroom", "stone", "shroom_wood"],
+    ["pollen_shroom", "cave_flower", "fibres"],
+    ["bronze_berry","fibres","shroom_wood","bronze_pickaxe"],
+    ["bronze_berry","fibres","shroom_wood","bronze_sickle"],
+    ["stone","bronze_berry","fibres","shroom_wood","bronze_chisel"],
 ]
 
 var initPack = {player:[],bullet:[],floof:[]}
@@ -119,20 +164,20 @@ Player = function(id, username, socket, progress){
     self.hotbar = ["Nothing", "Nothing", "Nothing", "Nothing", "Nothing"]
     self.loadedChunks = []
     self.lookingRight = false
-    self.spriteId = undefined
+    self.spriteId = [0, 0]
     self.currentRightClick = 0
     self.lastRightClick = 0
     self.currentLeftClick = 0
     self.lastLeftClick = 0
     self.tileDestroyState = 0
+    self.currentTileStrength = 100
 
-    self.inventory.addItem("survival_knife", 1)
-    self.inventory.addItem("bronze_pickaxe", 1)
-    self.inventory.addItem("iron_pickaxe", 1)
-    self.inventory.addItem("rock", 32)
+    self.inventory.addItem("iron_drill", 1)
+    self.inventory.addItem("bronze_chisel", 1)
+    self.inventory.addItem("bronze_sickle", 1)
     self.inventory.addItem("shroom_k", 1)
     self.inventory.addItem("hunting_rifle", 1)
-    self.inventory.addItem("almond_water", 15)
+    self.inventory.addItem("survival_knife", 1)
 
     var superUpdate = self.update;
     self.update = function(){
@@ -146,14 +191,14 @@ Player = function(id, username, socket, progress){
         self.mouseCanvasX = self.mouseX + self.x - 900 // 900 = 1800/2
         self.mouseCanvasY = self.mouseY + self.y - 480 // 480 = 960/2
 
-        let currentChunk = world.getChunk(Math.floor((self.x / 50) / 32), Math.floor((self.y / 50) / 32))
+        let currentChunk = world.getChunk(Math.floor((self.x / tpd) / ctd), Math.floor((self.y / tpd) / ctd))
         
-        let currentMouseChunk = world.getChunk(Math.floor((self.mouseCanvasX / 50) / 32), Math.floor((self.mouseCanvasY / 50) / 32))
-        let mouseXInChunk = Math.floor(self.mouseCanvasX / 50 - currentMouseChunk.x * 32)
-        let mouseYInChunk = Math.floor(self.mouseCanvasY / 50 - currentMouseChunk.y * 32)
+        let currentMouseChunk = world.getChunk(Math.floor((self.mouseCanvasX / tpd) / ctd), Math.floor((self.mouseCanvasY / tpd) / ctd))
+        let mouseXInChunk = Math.floor(self.mouseCanvasX / tpd - currentMouseChunk.x * ctd)
+        let mouseYInChunk = Math.floor(self.mouseCanvasY / tpd - currentMouseChunk.y * ctd)
 
-        let mouseChunkX = Math.floor((self.mouseCanvasX / 50) / 32)
-        let mouseChunkY = Math.floor((self.mouseCanvasY / 50) / 32)
+        let mouseChunkX = Math.floor((self.mouseCanvasX / tpd) / ctd)
+        let mouseChunkY = Math.floor((self.mouseCanvasY / tpd) / ctd)
         let mouseChunkidx = (mouseChunkX << 16) | mouseChunkY
 
         let tileToPlace = 0
@@ -161,7 +206,7 @@ Player = function(id, username, socket, progress){
         if(self.hotbar[self.activeSlot] !== "Nothing")
             self.spriteId = self.inventory.getItemSpriteId(self.hotbar[self.activeSlot])
         else
-            self.spriteId = 0
+            self.spriteId = [0, 0]
         //console.log(self.inventory.hasItem(self.hotbar[self.activeSlot], 1))
 
         getTile = function(xic, yic){
@@ -193,21 +238,39 @@ Player = function(id, username, socket, progress){
             }      
 
             if(singleGuns.includes(self.hotbar[self.activeSlot])){
-                self.shootBullet(self.mouseAngle, 50)
+                self.shootBullet(self.mouseAngle, 50, 5)
             }
 
             self.lastRightClick = self.currentRightClick
         }
 
         if(self.holdingMouseLeft){
-            if(miningTools.includes(self.hotbar[self.activeSlot]) && getTile(mouseXInChunk, mouseYInChunk) !== 1){
-                self.tileDestroyState += 1 * miningToolStrengths[self.hotbar[self.activeSlot]]
-                if(self.tileDestroyState >= 50){
-                    if(inverse(placeIds)[getTile(mouseXInChunk, mouseYInChunk)] !== undefined)
-                        self.inventory.addItem(inverse(placeIds)[getTile(mouseXInChunk, mouseYInChunk)], 1)
+            let toolType = "none"
+            let selectedTile = getTile(mouseXInChunk, mouseYInChunk)
+            if(mineTiles.includes(selectedTile))
+                toolType = [miningToolStrengths, miningTools]
+            else if(harvestTiles.includes(selectedTile))
+                toolType = [harvestToolStrengths, harvestTools]
+            else if(workTiles.includes(selectedTile))
+                toolType = [workToolStrengths, workTools]
+
+            self.currentTileStrength = tileStrengths[selectedTile]
+
+            if(toolType[1].includes(self.hotbar[self.activeSlot]) && selectedTile !== 1){
+                self.tileDestroyState += 1 * toolType[0][self.hotbar[self.activeSlot]]
+                if(self.tileDestroyState >= tileStrengths[selectedTile]){
+                    if(inverse(placeIds)[selectedTile] !== undefined)
+                        self.inventory.addItem(inverse(placeIds)[selectedTile], 1)
 
                     currentMouseChunk.tiles[mouseYInChunk * currentMouseChunk.width + mouseXInChunk] = 1
-                    tileToPlace = 1
+                    if (floor1Tiles.includes(selectedTile))
+                        tileToPlace = 1
+                    else if (floor2Tiles.includes(selectedTile))
+                        tileToPlace = 12
+                    else if (floor3Tiles.includes(selectedTile))
+                        tileToPlace = 9
+                    else
+                        tileToPlace = 1
 
                     socket.broadcast.emit('tile-change',{
                         tileToPlace: tileToPlace,
@@ -245,11 +308,11 @@ Player = function(id, username, socket, progress){
 
         if(self.holdingMouseRight){
             if(autoGuns.includes(self.hotbar[self.activeSlot])){
-                self.shootBullet(self.mouseAngle, 8)
+                self.shootBullet(self.mouseAngle, 8, 1)
             }
 
             if(meleeWeapons.includes(self.hotbar[self.activeSlot])){
-                self.meleeAttack(self.mouseAngle)
+                self.meleeAttack(self.mouseAngle, 1, 2)
             }
 
             if(placeableItems.includes(self.hotbar[self.activeSlot]) && !priorityTiles.includes(getTile(mouseXInChunk, mouseYInChunk))){
@@ -274,22 +337,22 @@ Player = function(id, username, socket, progress){
         }
     }
 
-    self.shootBullet = function(angle, lifetime){
-        var b = Bullet(self.id, angle, lifetime, 32)
+    self.shootBullet = function(angle, lifetime, damage){
+        var b = Bullet(self.id, angle, lifetime, 32, damage)
         b.x = self.x
         b.y = self.y
     }
 
-    self.meleeAttack = function(angle){
-        var b = Bullet(self.id, angle, 1, 32)
+    self.meleeAttack = function(angle, damage){
+        var b = Bullet(self.id, angle, 1, 32, damage)
         b.x = self.x
         b.y = self.y
     }
 
     self.updateSpeed = function(){
-        let currentChunk = world.getChunk(Math.floor((self.x / 50) / 32), Math.floor((self.y / 50) / 32))
-        let xInChunk = Math.floor(self.x / 50 - currentChunk.x * 32)
-        let yInChunk = Math.floor(self.y / 50 - currentChunk.y * 32)
+        let currentChunk = world.getChunk(Math.floor((self.x / tpd) / ctd), Math.floor((self.y / tpd) / ctd))
+        let xInChunk = Math.floor(self.x / tpd - currentChunk.x * ctd)
+        let yInChunk = Math.floor(self.y / tpd - currentChunk.y * ctd)
 
         getTile = function(xic, yic){
             return currentChunk.tiles[yic * currentChunk.width + xic]
@@ -357,17 +420,18 @@ Player = function(id, username, socket, progress){
             hpMax:self.hpMax,
             score:self.score,
             effects:self.effects,
-            chunk:world.getChunk(Math.floor((self.x / 50) / 32), Math.floor((self.y / 50) / 32)),
+            chunk:world.getChunk(Math.floor((self.x / tpd) / ctd), Math.floor((self.y / tpd) / ctd)),
             hotbar:self.hotbar,
             activeSlot:self.activeSlot,
             lookingRight:self.lookingRight,
             spriteId:self.spriteId,
             tileDestroyState:self.tileDestroyState,
+            currentTileStrength:self.currentTileStrength,
         }
     }
     self.getUpdatePack = function(){
-        let chunkx = Math.floor((self.x / 50) / 32)
-        let chunky = Math.floor((self.y / 50) / 32)
+        let chunkx = Math.floor((self.x / tpd) / ctd)
+        let chunky = Math.floor((self.y / tpd) / ctd)
         let chunkToSend = []
 
         for (let dx = -1; dx < 2; dx++) {
@@ -406,6 +470,7 @@ Player = function(id, username, socket, progress){
             lookingRight:self.lookingRight,
             spriteId:self.spriteId,
             tileDestroyState:self.tileDestroyState,
+            currentTileStrength:self.currentTileStrength,
         }
     }
 
@@ -518,7 +583,7 @@ Player.update = function(){
 
 // Bullet -----------------------------------------------------------------------
 {
-Bullet = function(parent, angle, lifetime, size){
+Bullet = function(parent, angle, lifetime, size, damage){
     var self = Entity()
     self.id = Math.random()
     self.speedX = Math.cos(angle/180*Math.PI) * 45
@@ -526,6 +591,7 @@ Bullet = function(parent, angle, lifetime, size){
     self.parent = parent
     self.timer = 0
     self.toRemove = false
+    self.damage = damage
 
     var superUpdate = self.update
     self.update = function(){
@@ -537,7 +603,7 @@ Bullet = function(parent, angle, lifetime, size){
         for (var i in Player.list){
             var p = Player.list[i]
             if(self.getDistance(p) < size && self.parent !== p.id){
-                p.hp -= 1
+                p.hp -= self.damage
                 if(p.hp <= 0){
                     var shooter = Player.list[self.parent]
                     if(shooter){
@@ -549,9 +615,9 @@ Bullet = function(parent, angle, lifetime, size){
             }
         }
 
-        let currentChunk = world.getChunk(Math.floor((self.x / 50) / 32), Math.floor((self.y / 50) / 32))
-        let xInChunk = Math.floor(self.x / 50 - currentChunk.x * 32)
-        let yInChunk = Math.floor(self.y / 50 - currentChunk.y * 32)
+        let currentChunk = world.getChunk(Math.floor((self.x / tpd) / ctd), Math.floor((self.y / tpd) / ctd))
+        let xInChunk = Math.floor(self.x / tpd - currentChunk.x * ctd)
+        let yInChunk = Math.floor(self.y / tpd - currentChunk.y * ctd)
 
         getTile = function(xic, yic){
             return currentChunk.tiles[yic * currentChunk.width + xic]
@@ -574,6 +640,7 @@ Bullet = function(parent, angle, lifetime, size){
             id:self.id,
             x:self.x,
             y:self.y,
+            angle:angle,
         }
     }
     self.getUpdatePack = function(){
@@ -581,6 +648,7 @@ Bullet = function(parent, angle, lifetime, size){
             id:self.id,
             x:self.x,
             y:self.y,
+            angle:angle,
         }
     }
 
@@ -616,8 +684,8 @@ Bullet.getAllInitPack = function(){
 {
 Floof = function(){
     var self = Entity()
-    self.x = randomRange(480 * 50, 520 * 50)
-    self.y = randomRange(480 * 50, 520 * 50)
+    self.x = randomRange(24 * tpd, 1024 * tpd)
+    self.y = randomRange(24 * tpd, 1024 * tpd)
     self.id = Math.random()
     self.speedX = 0 
     self.speedY = 0
@@ -643,6 +711,7 @@ Floof = function(){
                 // handle collision
                 self.toRemove = true
                 Player.list[b.parent].inventory.addItem("medkit", 1)
+                Player.list[b.parent].inventory.addItem("floof_wool", 1)
                 /*
                 for(var i in SOCKET_LIST){
                     SOCKET_LIST[i].emit("addToChat", "floof " + self.number + " was killed")
@@ -651,9 +720,9 @@ Floof = function(){
             }
         }
 
-        let currentChunk = world.getChunk(Math.floor((self.x / 50) / 32), Math.floor((self.y / 50) / 32))
-        let xInChunk = Math.floor(self.x / 50 - currentChunk.x * 32)
-        let yInChunk = Math.floor(self.y / 50 - currentChunk.y * 32)
+        let currentChunk = world.getChunk(Math.floor((self.x / tpd) / ctd), Math.floor((self.y / tpd) / ctd))
+        let xInChunk = Math.floor(self.x / tpd - currentChunk.x * ctd)
+        let yInChunk = Math.floor(self.y / tpd - currentChunk.y * ctd)
 
         getTile = function(xic, yic){
             return currentChunk.tiles[yic * currentChunk.width + xic]
@@ -703,7 +772,7 @@ Floof.update = function(){
     for(var i in Floof.list){
         floofCount++
     }
-    if (Math.random() < 0.4 && floofCount < 20){
+    if (Math.random() < 0.4 && floofCount < 500){
         Floof()
     }
     
